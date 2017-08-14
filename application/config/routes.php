911efbcd13f453f71b11cf708c55524b5e739be2
Md.Rubel <dpirubel@hotmail.com>
2017-08-14 18:34:15 +0600

    Change route file

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+
+// Tanvir Routes
+
+
+
+// Rubel Routel
+
+
+
+// Default Routes
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
