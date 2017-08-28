ef729d07a0635db19c5a0770e51529f433850cda
Tanvir Sarker <tanvir603@gmail.com>
2017-08-28 13:39:03 +0600

    Add Branc Module

---
+++ application/modules/branch/controllers/Branch.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/* 
+	Module: Dashboard Module Section
+	Author: Nihal IT
+	Project: Micro-Credit
+	Start: August 2017 
+	Last Update: 27-Aug
+*/
+
+class Branch extends MX_Controller
+{
+    protected $uType;
+    
+    public function __construct()
+    {			
+        $this->checkSession();
+        $this->uType = returnUserType($_SESSION['userInfo']['userType']);
+    }    
+
+    public function index() 
+	{
+        $this->activeMenu('Branch');
+        $data = ['Branch','content','']; /* P1=TITLE|P2=PAGENAME|P3=PARAMITER */
+		$this->loadAllContent($data);	
+    }      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* MENDATORY FUNCTOINS MUST NEED EVERY MODULES CONTROLLER */
+
+    public function loadAllContent($dynamicContent)
+    {
+        /* LOAD DYNAMIC CONTENT */
+        $data['title'] = $dynamicContent[0];
+        $data['contents'] = $this->load->view($this->uType.'/'.$dynamicContent[1],$dynamicContent[2],true);
+        /* LOAD TEMPLATE MAIN CONTENT */
+        $data2['userType']      = $this->uType;
+        $data['adminHeaderSrc'] = $this->load->view('templeteSrc/headerSrc',$data2,true);
+		$data['adminHeader']    = $this->load->view('templeteSrc/header',$data2,true);
+		$data['adminSidebar']   = $this->load->view('templeteSrc/sidebar',$data2,true);             
+		$data['adminFooter']    = $this->load->view('templeteSrc/footer',$data2,true);
+        $data['adminFooterSrc'] = $this->load->view('templeteSrc/footerSrc',$data2,true);
+        $this->load->view('templeteSrc/master',$data);
+    }
+
+    public function activeMenu($data)
+    {
+        $_SESSION['menuOpen']   = $this->router->fetch_module();
+        $_SESSION['menuActive'] = $data;
+    }
+
+    public function checkSession()
+    {
+        if(empty($_SESSION['userInfo']))
+		{
+            $this->session->set_flashdata('error', 'Please input email & password.');
+			redirect(base_url());
+        }	
+    }
+
+}
